16e8b976e1b7fc95337399591f3a43e9fc227cdd
Race Briggs <race.briggs@gmail.com>
2018-11-06 14:36:17 -0600

    altered fizzbuzzer test

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -2,21 +2,21 @@ const expect = require('chai').expect;
 
 const fizzBuzzer = require('../fizzBuzzer');
 
-describe(fizzBuzzer, function() {
+describe('fizzBuzzer', function() {
 	it('should replace numbers divisible by 3, 5, or 15', function() {
-		const properInputs = [1, 2, 3, 4, 5, 7, 8, 10, 15];
+		const properInputs = [3, 5, 15];
 		properInputs.forEach(function(input){
 			const answer = fizzBuzzer(input);
-			expect(answer).to.be.oneOf([num, 'fizz', 'buzz', 'fizzbuzz']);
+			expect(answer).to.be.oneOf(['fizz', 'buzz', 'fizzbuzz']);
 		});
 	});
 
 
 	it('should trow an error when presented with something other than a number', function() {
-		const wrongInputs = [1, 2, 3, 5, 'apple', 10, 15];
+		const wrongInputs = ['apple'];
 		wrongInputs.forEach(function(input){
 			const answer = fizzBuzzer(input);
 			expect(answer).to.throw(Error);
 		});
 	});
-})
+});
